489c15a5aa25a858cba606813e3b07fbbd3ddda7
fattanazzaka <fattanazzaka25@gmail.com>
2025-11-18 23:04:31 +0700

    edit navbar tambah button login

--- udangtracker_project/urls.py
+++ udangtracker_project/urls.py
@@ -6,7 +6,6 @@ from tracker import views as tracker_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", tracker_views.lot_list, name="home"),
     path("", include("tracker.urls")),
     path("authenticate/", include("authenticate.urls")),
 ]
